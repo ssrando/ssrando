26afab7a7b0d24f35eb50f94f2f187e0c1f08ccc
lepelog <lepelog4@gmail.com>
2020-08-31 13:02:58 +0200

    more patches, objadd

--- layerfixes.py
+++ layerfixes.py
@@ -7,21 +7,6 @@ import json
 from sslib import AllPatcher
 from sslib.utils import write_bytes_create_dirs
 
-# orig_id, srclayer, srcroom, srcobjtype, destlayer, destroom, destobjtype
-object_moves = [
-    # skyview
-    # eye at start
-    (0xFC48, 1, 0),
-
-    # right eyes
-    (0xFC48, 1, 0),
-    (0xFC49, 1, 0),
-    (0xFC4A, 1, 0),
-    (0xFC49, 1, 0),
-    (0xFC49, 1, 0),
-    (0xFC5E, 1, 0),
-]
-
 def highest_objid(bzs):
     max_id = 0
     for layer in bzs.get('LAY ',{}).values():
@@ -30,7 +15,7 @@ def highest_objid(bzs):
         for objtype in ['OBJS','OBJ ','SOBS','SOBJ','STAS','STAG','SNDT','DOOR']:
             if objtype in layer:
                 id = layer[objtype][-1]['id'] & 0x3FF
-                if id != 0x3FF:
+                if id != 0x3FF: # aparently some objects have the max id? 
                     max_id = max(max_id, id)
     return max_id
 
@@ -69,39 +54,94 @@ def fix_layers():
                 bzs['LYSE'] = layer_override
                 modified = True
         next_id = highest_objid(bzs) + 1
-        for objmove in filter(lambda x: x['type']=='objmove' and x.get('srcroom',None)==room, stagepatches):
-            id = int(objmove['id'],16)
-            srclayer = objmove['srclayer']
+        for objmove in filter(lambda x: x['type']=='objmove' and x.get('room',None)==room, stagepatches):
+            id = objmove['id']
+            layer = objmove['layer']
             destlayer = objmove['destlayer']
-            srcobjtype = objmove['srcobjtype'].ljust(4) # OBJ has an whitespace but thats was too error prone for the yaml, so just pad it here
-            objs = [x for x in bzs['LAY '][f'l{srclayer}'][srcobjtype] if x['id'] == id]
+            objtype = objmove['objtype'].ljust(4) # OBJ has an whitespace but thats was too error prone for the yaml, so just pad it here
+            objs = [x for x in bzs['LAY '][f'l{layer}'][objtype] if x['id'] == id]
             if len(objs) != 1:
                 print(f'Error finding object: {json.dumps(objmove)}')
             else:
                 obj = objs[0]
-                bzs['LAY '][f'l{srclayer}'][srcobjtype].remove(obj)
+                bzs['LAY '][f'l{layer}'][objtype].remove(obj)
                 obj['id'] = (obj['id'] & ~0x3FF) | next_id
                 next_id += 1
-                bzs['LAY '][f'l{destlayer}'][srcobjtype].append(obj)
+                bzs['LAY '][f'l{destlayer}'][objtype].append(obj)
                 objn = bzs['LAY '][f'l{destlayer}']['OBJN']
                 if not obj['name'] in objn:
                     objn.append(obj['name'])
                 modified = True
-                print(f'moved object from {srclayer} to {destlayer} in room {room} with id {objmove["id"]}')
-                print(obj)
-        for objdelete in filter(lambda x: x['type']=='objdelete' and x.get('srcroom',None)==room, stagepatches):
-            id = int(objdelete['id'],16)
-            srclayer = objdelete['srclayer']
-            srcobjtype = objdelete['srcobjtype'].ljust(4) # OBJ has an whitespace but thats was too error prone for the yaml, so just pad it here
-            objs = [x for x in bzs['LAY '][f'l{srclayer}'][srcobjtype] if x['id'] == id]
+                print(f'moved object from {layer} to {destlayer} in room {room} with id {objmove["id"]}')
+                # print(obj)
+        for objdelete in filter(lambda x: x['type']=='objdelete' and x.get('room',None)==room, stagepatches):
+            id = objdelete['id']
+            layer = objdelete['layer']
+            objtype = objdelete['objtype'].ljust(4) # OBJ has an whitespace but thats was too error prone for the yaml, so just pad it here
+            objs = [x for x in bzs['LAY '][f'l{layer}'][objtype] if x['id'] == id]
             if len(objs) != 1:
                 print(f'Error finding object: {json.dumps(objdelete)}')
             else:
                 obj = objs[0]
-                bzs['LAY '][f'l{srclayer}'][srcobjtype].remove(obj)
+                bzs['LAY '][f'l{layer}'][objtype].remove(obj)
                 modified = True
-                print(f'removed object from {srclayer} in room {room} with id {objdelete["id"]}')
-                print(obj)
+                print(f'removed object from {layer} in room {room} with id {objdelete["id"]}')
+                # print(obj)
+        for objpatch in filter(lambda x: x['type']=='objpatch' and x.get('room',None)==room, stagepatches):
+            id = objpatch['id']
+            layer = objpatch['layer']
+            objtype = objpatch['objtype'].ljust(4) # OBJ has an whitespace but thats was too error prone for the yaml, so just pad it here
+            objs = [x for x in bzs['LAY '][f'l{layer}'][objtype] if x['id'] == id]
+            if len(objs) != 1:
+                print(f'Error finding object: {json.dumps(objpatch)}')
+            else:
+                obj = objs[0]
+                for key, val in objpatch['patch'].items():
+                    obj[key] = val
+                modified = True
+                print(f'modified object from {layer} in room {room} with id {objpatch["id"]}')
+                # print(obj)
+        for objadd in filter(lambda x: x['type']=='objadd' and x.get('room',None)==room, stagepatches):
+            layer = objadd['layer']
+            objtype = objadd['objtype'].ljust(4) # OBJ has an whitespace but thats was too error prone for the yaml, so just pad it here
+            obj = objadd['object']
+            if objtype in ['SOBS','SOBJ','STAS','STAG','SNDT']:
+                new_obj = OrderedDict(
+                    params1 = obj['params1'],
+                    params2 = obj['params2'],
+                    posx = obj['posx'],
+                    posy = obj['posy'],
+                    posz = obj['posz'],
+                    sizex = obj['sizex'],
+                    sizey = obj['sizey'],
+                    sizez = obj['sizez'],
+                    anglex = obj['anglex'],
+                    angley = obj['angley'],
+                    anglez = obj['anglez'],
+                    id = (obj['id'] & ~0x3FF) | next_id,
+                    name = obj['name'],
+                )
+            elif objtype in ['OBJS','OBJ ','DOOR']:
+                new_obj = OrderedDict(
+                    params1 = obj['params1'],
+                    params2 = obj['params2'],
+                    posx = obj['posx'],
+                    posy = obj['posy'],
+                    posz = obj['posz'],
+                    anglex = obj['anglex'],
+                    angley = obj['angley'],
+                    anglez = obj['anglez'],
+                    id = (obj['id'] & ~0x3FF) | next_id,
+                    name = obj['name'],
+                )
+            else:
+                print(f'Error: unknown objtype: {objtype}')
+                continue
+            next_id += 1
+            bzs['LAY '][f'l{layer}'][objtype].append(new_obj)
+            modified = True
+            print(f'added object {obj["name"]} to {layer} in room {room}')
+            # print(obj)
         if modified:
             # print(json.dumps(bzs))
             return bzs
