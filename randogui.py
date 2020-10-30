0e876a770dc52aeb000c412345c9b7fb28fb9315
cjs8487 <cjschaefer1999@gmail.com>
2020-10-30 17:50:33 -0400

    fetch and extract wit and change name of randomized iso

--- randogui.py
+++ randogui.py
@@ -1,12 +1,18 @@
 from collections import OrderedDict
+from io import BytesIO
 from pathlib import Path
 from tkinter import *
 from tkinter import ttk
 from threading import Thread
 from ssrando import Randomizer
+from urllib import request
 
 import subprocess
-import os
+import zipfile
+
+
+wit_url = "https://wit.wiimm.de/download/wit-v3.03a-r8245-cygwin.zip"
+wit_folder = "wit-v3.03a-r8245-cygwin"
 
 
 def randomize():
@@ -14,14 +20,20 @@ def randomize():
 
 
 def offthread_randomize():
-    if not (Path(".").parent / "actual-extract").is_dir():
-        subprocess.run(["wit", "-P", "extract", "disc.iso", "actual-extract"])
-    if not (Path(".").parent / "modified-extract").is_dir():
+    if not (Path(".") / wit_folder).is_dir():
+        # fetch and unzip wit dependency
+        with zipfile.ZipFile(BytesIO(request.urlopen(wit_url).read())) as wit_zip:
+            wit_zip.extractall(Path(".") / wit_folder)
+
+    if not (Path(".") / "actual-extract").is_dir():
+        subprocess.run([(Path(".") / wit_folder / "bin" / "wit").name, "-P", "extract", "disc.iso", "actual-extract"])
+    if not (Path(".") / "modified-extract").is_dir():
         subprocess.run(["xcopy", "/E", "/I", "actual-extract", "modified-extract"])
-    rando = Randomizer(OrderedDict([('dry-run', False), ('randomize-tablets', True), ('closed-thunderhead', False), ('swordless', False), ('invisible-sword', False), ('empty-unrequired-dungeons', True), ('banned-types', ''), ('seed', -1)]))
+    rando = Randomizer(OrderedDict([('dry-run', False), ('randomize-tablets', False), ('closed-thunderhead', True), ('swordless', False), ('invisible-sword', False), ('empty-unrequired-dungeons', True), ('banned-types', ''), ('seed', -1)]))
     print(rando.seed)
     rando.randomize()
-    subprocess.run(["wit", "-P", "copy", "modified-extract", "rando.iso"])
+    iso_name = "SS Randomizer " + str(rando.seed) + ".iso"
+    subprocess.run([(Path(".") / wit_folder / "bin" / "wit").name, "-P", "copy", "modified-extract", iso_name])
 
 
 root = Tk()
