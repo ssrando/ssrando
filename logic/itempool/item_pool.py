d7a36db21edc733b97d2b3408c17d510a1f018b8
cjs8487 <cjschaefer1999@gmail.com>
2021-06-22 18:50:02 -0400

    added full itempool lists

--- logic/itempool/item_pool.py
+++ logic/itempool/item_pool.py
@@ -16,6 +16,9 @@ class ItemPool:
     dungeon_nonprogress_items: list
     junk_items: list
 
+    all_progress_items: list
+    all_nonprogress_items: list
+
     def __init__(self, rando: Randomizer, progress_items: list, dungeon_progress_items: list, nonprogress_items: list,
                  dungeon_nonprogress_items, junk_items: list):
         self.rando = rando
@@ -24,6 +27,9 @@ class ItemPool:
         self.nonprogress_items = self.rando.rng.shuffle(nonprogress_items.copy())
         self.dungeon_nonprogress_items = self.rando.rng.shuffle(dungeon_nonprogress_items.copy())
         self.nonprogress_items = self.rando.rng.shuffle(junk_items.copy())
+
+        self.all_progress_items = progress_items + dungeon_progress_items
+        self.all_nonprogress_items = nonprogress_items + dungeon_nonprogress_items
 
     def get_nonprogress_item(self):
         """
@@ -67,12 +73,6 @@ class ItemPool:
         """
         return self.dungeon_nonprogress_items.pop()
 
-    def get_all_progress_items(self):
-        """
-        Returns the entire remaining progress item pool
-        """
-        return self.progress_items
-
     def update_progress_pool(self):
         """
         Updates the progress pool based on the settings for the randomizer
@@ -99,9 +99,11 @@ class ItemPool:
         # shuffle the item into the progress pool
         self.progress_items.append(item)
         self.rando.rng.shuffle(self.progress_items)
+        self.all_progress_items.append(item)
 
         # remove from the nonprogress pool (no need to reshuffle)
         self.nonprogress_items.remove(item)
+        self.all_nonprogress_items.remove(item)
 
     def remove_item(self, item):
         """
@@ -109,9 +111,15 @@ class ItemPool:
         """
         if item in self.progress_items:
             self.progress_items.remove(item)
+            self.all_progress_items.remove(item)
         elif item in self.dungeon_progress_items:
             self.dungeon_progress_items.remove(item)
+            self.all_progress_items.remove(item)
         elif item in self.nonprogress_items:
             self.nonprogress_items.remove(item)
+            self.all_nonprogress_items.remove(item)
+        elif item in self.dungeon_nonprogress_items:
+            self.dungeon_nonprogress_items.remove(item)
+            self.all_nonprogress_items.remove(item)
         else:  # if it hasn't appeared anywhere else, it is either junk or doesn't exist at all
             self.junk_items.remove(item)
