175287e203b33deeba4c29ee99ab3b9da0de6746
cjs8487 <cjschaefer1999@gmail.com>
2021-10-23 14:18:08 -0400

    format

--- logic/hints.py
+++ logic/hints.py
@@ -214,7 +214,8 @@ class Hints:
                 hints_left -= 1
                 location_hints_left -= 1
         for location in self.logic.rando.rng.sample(
-            needed_sometimes_hints, k=min(location_hints_left, len(needed_sometimes_hints))
+            needed_sometimes_hints,
+            k=min(location_hints_left, len(needed_sometimes_hints)),
         ):
             if location not in hinted_locations:
                 location_hints.append(location)
