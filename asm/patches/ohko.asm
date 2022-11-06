4d35a1e1a2a5fe2ecdaee5c3db37ffe0d3e58e3e
YourAverageLink <37936141+YourAverageLink@users.noreply.github.com>
2022-11-05 20:06:46 -0500

    make ohko actually work for quarter-heart damage

--- asm/patches/ohko.asm
+++ asm/patches/ohko.asm
@@ -1,6 +1,6 @@
 .open "main.dol"
 .org 0x801e346c ; checks for hero mode for damage doubling
 nop ; don't branch away if normal mode
-; shift damage 8 bits left (256x), ensures death even on normal mode + guardian potion
-rlwinm r27,r27,0x8,0x0,0x1e
+; load highest possible negative damage amount
+li r27, -32768
 .close
