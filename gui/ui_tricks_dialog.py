5c748b3e2cd6aced2e9effb98009a246800bd3d9
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2023-02-16 10:55:28 +0000

    Fix UI scaling

--- gui/ui_tricks_dialog.py
+++ gui/ui_tricks_dialog.py
@@ -16,54 +16,96 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QHBoxLayout, QListView, QPushButton, QSizePolicy,
-    QVBoxLayout, QWidget)
+    QHBoxLayout, QLabel, QListView, QPushButton,
+    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_TricksDialog(object):
     def setupUi(self, TricksDialog):
         if not TricksDialog.objectName():
             TricksDialog.setObjectName(u"TricksDialog")
-        TricksDialog.resize(1005, 309)
-        self.bbox_tricks = QDialogButtonBox(TricksDialog)
-        self.bbox_tricks.setObjectName(u"bbox_tricks")
-        self.bbox_tricks.setGeometry(QRect(660, 270, 341, 32))
-        self.bbox_tricks.setOrientation(Qt.Horizontal)
-        self.bbox_tricks.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
-        self.layoutWidget = QWidget(TricksDialog)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(8, 10, 991, 251))
-        self.vlay_tricks = QVBoxLayout(self.layoutWidget)
+        TricksDialog.resize(1005, 481)
+        self.verticalLayout = QVBoxLayout(TricksDialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.vlay_tricks = QVBoxLayout()
         self.vlay_tricks.setObjectName(u"vlay_tricks")
-        self.vlay_tricks.setContentsMargins(0, 0, 0, 0)
         self.hlay_tricks_body = QHBoxLayout()
         self.hlay_tricks_body.setObjectName(u"hlay_tricks_body")
-        self.disabled_tricks = QListView(self.layoutWidget)
+        self.vlay_disabled_tricks = QVBoxLayout()
+        self.vlay_disabled_tricks.setObjectName(u"vlay_disabled_tricks")
+        self.label_for_disabled_tricks = QLabel(TricksDialog)
+        self.label_for_disabled_tricks.setObjectName(u"label_for_disabled_tricks")
+
+        self.vlay_disabled_tricks.addWidget(self.label_for_disabled_tricks)
+
+        self.disabled_tricks = QListView(TricksDialog)
         self.disabled_tricks.setObjectName(u"disabled_tricks")
 
-        self.hlay_tricks_body.addWidget(self.disabled_tricks)
+        self.vlay_disabled_tricks.addWidget(self.disabled_tricks)
+
+
+        self.hlay_tricks_body.addLayout(self.vlay_disabled_tricks)
 
         self.vlay_tricks_controls = QVBoxLayout()
         self.vlay_tricks_controls.setObjectName(u"vlay_tricks_controls")
-        self.disable_trick = QPushButton(self.layoutWidget)
+        self.vspace_tricks_controls_upper = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.vlay_tricks_controls.addItem(self.vspace_tricks_controls_upper)
+
+        self.disable_trick = QPushButton(TricksDialog)
         self.disable_trick.setObjectName(u"disable_trick")
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.disable_trick.sizePolicy().hasHeightForWidth())
+        self.disable_trick.setSizePolicy(sizePolicy)
 
         self.vlay_tricks_controls.addWidget(self.disable_trick)
 
-        self.enable_trick = QPushButton(self.layoutWidget)
+        self.vspace_tricks_controls_middle = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.vlay_tricks_controls.addItem(self.vspace_tricks_controls_middle)
+
+        self.enable_trick = QPushButton(TricksDialog)
         self.enable_trick.setObjectName(u"enable_trick")
+        sizePolicy.setHeightForWidth(self.enable_trick.sizePolicy().hasHeightForWidth())
+        self.enable_trick.setSizePolicy(sizePolicy)
 
         self.vlay_tricks_controls.addWidget(self.enable_trick)
+
+        self.vspace_tricks_controls_lower = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.vlay_tricks_controls.addItem(self.vspace_tricks_controls_lower)
 
 
         self.hlay_tricks_body.addLayout(self.vlay_tricks_controls)
 
-        self.enabled_tricks = QListView(self.layoutWidget)
+        self.vlay_enabled_tricks = QVBoxLayout()
+        self.vlay_enabled_tricks.setObjectName(u"vlay_enabled_tricks")
+        self.label_for_enabled_tricks = QLabel(TricksDialog)
+        self.label_for_enabled_tricks.setObjectName(u"label_for_enabled_tricks")
+
+        self.vlay_enabled_tricks.addWidget(self.label_for_enabled_tricks)
+
+        self.enabled_tricks = QListView(TricksDialog)
         self.enabled_tricks.setObjectName(u"enabled_tricks")
 
-        self.hlay_tricks_body.addWidget(self.enabled_tricks)
+        self.vlay_enabled_tricks.addWidget(self.enabled_tricks)
+
+
+        self.hlay_tricks_body.addLayout(self.vlay_enabled_tricks)
 
 
         self.vlay_tricks.addLayout(self.hlay_tricks_body)
+
+
+        self.verticalLayout.addLayout(self.vlay_tricks)
+
+        self.bbox_tricks = QDialogButtonBox(TricksDialog)
+        self.bbox_tricks.setObjectName(u"bbox_tricks")
+        self.bbox_tricks.setOrientation(Qt.Horizontal)
+        self.bbox_tricks.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+
+        self.verticalLayout.addWidget(self.bbox_tricks)
 
 
         self.retranslateUi(TricksDialog)
@@ -75,6 +117,7 @@ class Ui_TricksDialog(object):
 
     def retranslateUi(self, TricksDialog):
         TricksDialog.setWindowTitle(QCoreApplication.translate("TricksDialog", u"Enable Tricks", None))
+        self.label_for_disabled_tricks.setText(QCoreApplication.translate("TricksDialog", u"Disabled Tricks", None))
 #if QT_CONFIG(tooltip)
         self.disabled_tricks.setToolTip(QCoreApplication.translate("TricksDialog", u"test", None))
 #endif // QT_CONFIG(tooltip)
@@ -82,5 +125,6 @@ class Ui_TricksDialog(object):
 "<--", None))
         self.enable_trick.setText(QCoreApplication.translate("TricksDialog", u"Enable\n"
 "-->", None))
+        self.label_for_enabled_tricks.setText(QCoreApplication.translate("TricksDialog", u"Enabled Tricks", None))
     # retranslateUi
 
