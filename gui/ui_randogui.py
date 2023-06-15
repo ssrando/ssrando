af1fbb9fa701bd76806667ae8ebb917c635b9553
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2023-06-15 18:13:31 +0100

    Squashed commit of the following:
    commit 63a1527f50d09a14e4e36c21454a2de107404f90
    Merge: ca444ebb 0efd6089
    Author: Lilith Harwood <48162682+Muzugalium@users.noreply.github.com>
    Date:   Thu Jun 15 17:10:15 2023 +0000
    
        Merge pull request #6 from YourAverageLink/fslavagui
    
        Merge main, add option & ui
    
    commit 0efd60898e8afd6028abd85a71580318e3cd8c8e
    Author: YourAverageLink <37936141+YourAverageLink@users.noreply.github.com>
    Date:   Thu Jun 15 11:58:45 2023 -0500
    
        merge main, make option & ui
    
    commit 9b4b66ad1e54ca15fe4a09edf2c6f8c019820dbf
    Merge: ca444ebb 530ff63d
    Author: YourAverageLink <37936141+YourAverageLink@users.noreply.github.com>
    Date:   Thu Jun 15 10:43:39 2023 -0500
    
        Merge remote-tracking branch 'upstream/main' into fslavagui
    
    commit 530ff63d2af40953948635c3d59c8786cdc9abe8
    Author: CovenEsme <84377742+CovenEsme@users.noreply.github.com>
    Date:   Thu Jun 15 10:34:54 2023 +0000
    
        Fix missed bug with change to rupeesanity option (#422)
    
    commit ca444ebb7ee99207e9c8a92b01967d714a83b770
    Merge: 31f631d7 f6bc4ac2
    Author: Lilith Harwood <48162682+Muzugalium@users.noreply.github.com>
    Date:   Wed Apr 19 21:56:25 2023 +0000
    
        Merge branch 'ssrando:main' into FSBackHalfLavaFlowStartFlag
    
    commit 31f631d7ecf2465d6e2ad2d74e0b8b5d7ba82ac1
    Author: Muzugalium <48162682+Muzugalium@users.noreply.github.com>
    Date:   Sun Jan 1 00:22:47 2023 +0000
    
        Set FS back half lava flow start flag

--- gui/ui_randogui.py
+++ gui/ui_randogui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'randogui.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.5.0
+## Created by: Qt User Interface Compiler version 6.2.4
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -712,6 +712,16 @@ class Ui_MainWindow(object):
         self.option_randomize_boss_key_puzzles.setObjectName(u"option_randomize_boss_key_puzzles")
 
         self.vlay_dungeons.addWidget(self.option_randomize_boss_key_puzzles)
+
+        self.label_for_dungeon_patches = QLabel(self.box_dungeons)
+        self.label_for_dungeon_patches.setObjectName(u"label_for_dungeon_patches")
+
+        self.vlay_dungeons.addWidget(self.label_for_dungeon_patches)
+
+        self.option_fs_lava_flow = QCheckBox(self.box_dungeons)
+        self.option_fs_lava_flow.setObjectName(u"option_fs_lava_flow")
+
+        self.vlay_dungeons.addWidget(self.option_fs_lava_flow)
 
         self.vspace_dungeons = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
@@ -1965,6 +1975,8 @@ class Ui_MainWindow(object):
         self.option_empty_unrequired_dungeons.setText(QCoreApplication.translate("MainWindow", u"Empty Unrequired Dungeons", None))
         self.label_for_sword_dungeon_reward.setText(QCoreApplication.translate("MainWindow", u"Sword Dungeon Reward", None))
         self.option_randomize_boss_key_puzzles.setText(QCoreApplication.translate("MainWindow", u"Randomize Boss Key Puzzles", None))
+        self.label_for_dungeon_patches.setText(QCoreApplication.translate("MainWindow", u"Dungeon Convenience Patches", None))
+        self.option_fs_lava_flow.setText(QCoreApplication.translate("MainWindow", u"Start w/ FS Lava Flowing", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_randomization_settings), QCoreApplication.translate("MainWindow", u"Randomization Settings", None))
         self.box_convenience_tweaks.setTitle(QCoreApplication.translate("MainWindow", u"Convenience Tweaks", None))
         self.option_fill_dowsing_on_white_sword.setText(QCoreApplication.translate("MainWindow", u"Fill Dowsing on White Sword", None))
