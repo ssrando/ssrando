d091c07ff4967c05e57cb17669f600d9e80185d0
lepelog <25211966+lepelog@users.noreply.github.com>
2024-03-06 00:38:18 +0100

    implement alloc and print for custom functions

--- asm/custom-functions/src/lib.rs
+++ asm/custom-functions/src/lib.rs
@@ -1,14 +1,41 @@
 #![no_std]
 #![feature(split_array)]
+#![feature(allocator_api)]
+#![feature(ascii_char)]
+#![feature(format_args_nl)]
 #![allow(dead_code)]
 #![deny(clippy::no_mangle_with_rust_abi)]
 #![deny(improper_ctypes)]
 #![deny(improper_ctypes_definitions)]
 
+use system::heap::DefaultGlobalAllocator;
+
+extern crate alloc;
+
+#[global_allocator]
+static DUMMY_ALLOC: DefaultGlobalAllocator = DefaultGlobalAllocator;
+
 mod game;
 mod rando;
 mod system;
 mod utils;
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {{
+        $crate::utils::printf::debug_print(format_args!($($arg)*));
+    }};
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {
+        $crate::print!("\n")
+    };
+    ($($arg:tt)*) => {{
+        $crate::utils::printf::debug_print(format_args_nl!($($arg)*));
+    }};
+}
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
--- asm/custom-functions/src/system/mod.rs
+++ asm/custom-functions/src/system/mod.rs
@@ -1,6 +1,7 @@
 pub mod button;
 pub mod gx;
 pub mod heap;
+pub mod ios;
 pub mod math;
 pub mod ppc;
 
