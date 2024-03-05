90d0c4e23e9bda6528a28a7c1045895db6f6075d
lepelog <25211966+lepelog@users.noreply.github.com>
2024-03-06 00:38:18 +0100

    implement alloc and print for custom functions

--- asm/custom-functions/src/utils/mod.rs
+++ asm/custom-functions/src/utils/mod.rs
@@ -2,6 +2,7 @@ pub mod char_writer;
 pub mod console;
 pub mod graphics;
 pub mod menu;
+pub mod printf;
 
 pub fn simple_rng(rng: &mut u32) -> u32 {
     *rng = rng.wrapping_mul(1664525).wrapping_add(1013904223);
