0a458ad59991fe74c281229208a41c44b156f6e1
lepelog <25211966+lepelog@users.noreply.github.com>
2024-03-06 00:38:18 +0100

    implement alloc and print for custom functions

--- asm/custom-functions/src/system/heap.rs
+++ asm/custom-functions/src/system/heap.rs
@@ -1,5 +1,9 @@
 #![allow(non_snake_case)]
-use core::ffi::{c_char, c_void, CStr};
+use core::{
+    alloc::{Allocator, GlobalAlloc},
+    ffi::{c_char, c_int, c_uint, c_void, CStr},
+    ptr::{null_mut, NonNull},
+};
 
 #[repr(C)]
 pub struct List {
@@ -33,7 +37,8 @@ extern "C" {
     static mut CURRENT_HEAP: *mut Heap;
     static HEAP_MEM1: *mut Heap;
     static HEAP_MEM2: *mut Heap;
-    fn Heap__alloc(size: u32, align: u32, heap: *const Heap) -> *mut c_void;
+    fn Heap__alloc(size: c_uint, align: c_int, heap: *mut Heap) -> *mut c_void;
+    fn Heap__free(ptr: *const c_void, heap: *mut Heap);
 }
 
 pub fn get_root_heap_mem1() -> *mut Heap {
@@ -42,4 +47,43 @@ pub fn get_root_heap_mem1() -> *mut Heap {
 
 pub fn get_root_heap_mem2() -> *mut Heap {
     unsafe { HEAP_MEM2 }
+}
+
+pub struct WiiHeapAllocator(*mut Heap);
+
+unsafe impl Allocator for WiiHeapAllocator {
+    fn allocate(
+        &self,
+        layout: core::alloc::Layout,
+    ) -> Result<core::ptr::NonNull<[u8]>, core::alloc::AllocError> {
+        let ptr =
+            unsafe { Heap__alloc(layout.size() as c_uint, layout.align() as c_int, self.0).cast() };
+        let ret = core::ptr::NonNull::new(ptr).ok_or(core::alloc::AllocError)?;
+        Ok(NonNull::slice_from_raw_parts(ret, layout.size()))
+    }
+
+    unsafe fn deallocate(&self, ptr: core::ptr::NonNull<u8>, _layout: core::alloc::Layout) {
+        Heap__free(ptr.as_ptr().cast(), self.0);
+    }
+
+    fn allocate_zeroed(
+        &self,
+        layout: core::alloc::Layout,
+    ) -> Result<NonNull<[u8]>, core::alloc::AllocError> {
+        // the default is already zero allocating
+        // TODO: is that actually true for all heaps?
+        self.allocate(layout)
+    }
+}
+
+pub struct DefaultGlobalAllocator;
+
+unsafe impl GlobalAlloc for DefaultGlobalAllocator {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        Heap__alloc(layout.size() as c_uint, layout.align() as c_int, null_mut()).cast()
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: core::alloc::Layout) {
+        Heap__free(ptr.cast(), null_mut())
+    }
 }
