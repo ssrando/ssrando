56db45e012a42e7efb439790fa7f49d43488a655
CelestalKitsune <30158363+CelestialKitsune@users.noreply.github.com>
2024-11-17 18:06:31 +0100

    Move all the spinboxes below their labels

--- gui/dialogs/tricks/ui_tricks_dialog.py
+++ gui/dialogs/tricks/ui_tricks_dialog.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'tricks_dialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.6.2
+## Created by: Qt User Interface Compiler version 6.8.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -47,7 +47,7 @@ class Ui_TricksDialog(object):
         self.disabled_tricks = QListView(TricksDialog)
         self.disabled_tricks.setObjectName(u"disabled_tricks")
         self.disabled_tricks.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.disabled_tricks.setProperty("showDropIndicator", False)
+        self.disabled_tricks.setProperty(u"showDropIndicator", False)
         self.disabled_tricks.setSelectionMode(QAbstractItemView.MultiSelection)
         self.disabled_tricks.setSelectionRectVisible(False)
 
@@ -106,7 +106,7 @@ class Ui_TricksDialog(object):
         self.enabled_tricks = QListView(TricksDialog)
         self.enabled_tricks.setObjectName(u"enabled_tricks")
         self.enabled_tricks.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.enabled_tricks.setProperty("showDropIndicator", False)
+        self.enabled_tricks.setProperty(u"showDropIndicator", False)
         self.enabled_tricks.setSelectionMode(QAbstractItemView.MultiSelection)
         self.enabled_tricks.setSelectionRectVisible(False)
 
